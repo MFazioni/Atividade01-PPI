81a952c9a0936b80b18158bdfe228f6afbc04a82
MFazioni <matheus_fazioni@hotmail.com>
2024-08-21 09:42:46 -0300

    atualizandoi

--- publico/script.js
+++ publico/script.js
@@ -15,3 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const quantityInput = document.getElementById("quantity2");
+    const totalSpan = document.getElementById("total");
+    const pricePerTicket = 35.00;
+
+
+    quantityInput.addEventListener("input", function() {
+        const quantity = parseInt(quantityInput.value);
+        if (isNaN(quantity)) {
+            totalSpan.textContent = "0.00"; 
+        } else {
+            const total = quantity * pricePerTicket;
+            totalSpan.textContent = total.toFixed(2);
+        }
+    });
+});
